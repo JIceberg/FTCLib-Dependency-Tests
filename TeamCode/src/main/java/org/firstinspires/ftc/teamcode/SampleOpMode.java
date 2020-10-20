9f3244c007164e25b2b1590f1a6338560c1a4565
JIceberg <jisenberg1050@gmail.com>
2020-10-20 13:28:13 -0400

    feat: abomination of a trigger binding

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleOpMode.java
@@ -1,12 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 
 public class SampleOpMode extends CommandOpMode {
 
     @Override
     public void initialize() {
-
+        // oh god no
+        GamepadButton button =
+                (GamepadButton) new GamepadButton(driverOp, GamepadKeys.Button.X, GamepadKeys.Button.Y)
+                        .and(
+                                new GamepadButton(toolOp, GamepadKeys.Button.A)
+                                        .whenActive(new InstantCommand())
+                        ).or(
+                        new GamepadButton(driverOp, GamepadKeys.Button.LEFT_BUMPER)
+                                .whileActiveContinuous(new InstantCommand())
+                ).negate().whenActive(new InstantCommand());
     }
 
 }
