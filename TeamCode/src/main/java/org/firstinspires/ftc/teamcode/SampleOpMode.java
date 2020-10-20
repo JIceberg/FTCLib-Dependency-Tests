de2866c356ba231fa7dce27feb2a7e8c15b52346
JIceberg <jisenberg1050@gmail.com>
2020-10-20 00:02:38 -0400

    feat: testing gradle builds

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleOpMode.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+
+public class SampleOpMode extends CommandOpMode {
+
+    @Override
+    public void initialize() {
+
+    }
+
+}
